f62e494100141dbb8237b1bf684fd76808d81f70
mohAttallah <mohmohmoud28@gmail.com>
2023-08-05 22:55:52 +0000

    try fix test

--- src/__test__/profile/profile.test.js
+++ src/__test__/profile/profile.test.js
@@ -5,20 +5,18 @@ const { app } = require("../../server");
 const supertest = require('supertest')
 const req = supertest(app)
 
-beforeAll(async connected => {
-    await newSequlize.sync();
+beforeAll(async()  => {
+    
     await modules.newUserCOll.create({ username: 'hamza', password: "123", email: "moh@getMaxListeners.com" })
     await modules.newUserCOll.create({ username: 'sham', password: "123", email: "moh@getMaxListeners.com" })
     await modules.newPostCOll.create({ imgurl: "moh@getMaxListeners.com", userid: "1", title: "nice photo" })
     await modules.bioCollection.create({ "contant": "welcome", userid: "1", })
     await modules.FollowersColl.create({ "following_id": 1, "me_id": 2 })
     await modules.FollowersColl.create({ "following_id": 2, "me_id": 1 })
-
-    connected();
+    await newSequlize.sync();
 })
 afterAll(async done => {
     await newSequlize.drop();
-    done();
 });
 
 
