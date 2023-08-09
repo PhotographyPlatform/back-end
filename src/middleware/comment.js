a8abd6f78d2d4fbdd526de380a44f0b9ff07e08d
mohAttallah <mohmohmoud28@gmail.com>
2023-08-09 08:58:00 +0000

    add post Update notification

--- src/middleware/comment.js
+++ src/middleware/comment.js
@@ -1,3 +1,4 @@
+const { where } = require('sequelize');
 const modules = require('../models')
 
 async function handleComment(req, res, next) {
@@ -8,10 +9,8 @@ async function handleComment(req, res, next) {
         const commentOwner = await modules.user.findByPk(respons.dataValues.userid);
         const postOwner = await modules.user.findByPk(respons.dataValues.postid); // Assuming the user with ID "like.postid" exists
         if (respons) {
-            modules.notificationCollection.create({
-                message: `${commentOwner.username} Add comment on your post`,
-                userid: postOwner.id
-            })
+            const message = `${commentOwner.username} Add comment on your post`;
+            reateNotificationRecord(postOwner.id, message);
         }
         res.status(201).json(respons);
 
@@ -24,23 +23,41 @@ async function handleFollowing(req, res, next) {
     try {
         const record = req.body;
         const respons = await modules.FollowersColl.create(record);
-
-        console.log(respons);
-
         const followingOwner = await modules.user.findByPk(respons.dataValues.following_id);
         const followersOwner = await modules.user.findByPk(respons.dataValues.me_id);
-
         if (respons) {
-            modules.notificationCollection.create({
-                message:`${followersOwner.username} followed you`,
-                userid:followingOwner.id
-            })
+            const message = `${followersOwner.username} followed you`;
+            createNotificationRecord(followingOwner.id, message)
         }
-
         res.status(201).json(respons);
     } catch (err) {
         next(err)
     }
+}
+
+async function handlePost(req, res, next) {
+    try {
+    const record = req.body;
+    const respons = await modules.newPostCOll.create(record);
+    const postOwner = await modules.user.findByPk(respons.dataValues.userid);
+    const followingPostOwner = await modules.Followers.findAll({ where: { following_id: postOwner.dataValues.id } });
+    console.log("++++++++++++++++++++++++++++++++++++++++++")
+    followingPostOwner.map(ele => {
+        let message = `${postOwner.username} added new photo`;
+        console.log(ele.me_id)
+        createNotificationRecord(ele.me_id, message)
+    });
+    res.status(201).json(respons); 
+    } catch (err) {
+        next(err)
+    }
+}
+
+async function createNotificationRecord(ownerid, message) {
+    await modules.notificationCollection.create({
+        message: message,
+        userid: ownerid
+    })
 }
 
 
@@ -48,5 +65,4 @@ async function handleFollowing(req, res, next) {
 
 
 
-
-module.exports = { handleComment, handleFollowing };
+module.exports = { handleComment, handleFollowing, handlePost };
--- src/routes/v1.js
+++ src/routes/v1.js
@@ -2,8 +2,8 @@ const express = require('express')
 const modules = require('../models')
 const v1Route = express.Router()
 const middleware = require('../middleware/basicRoutes');
-// const { handlecomment, handleFollowing } = require('../middleware/comment')
-const { handleComment, handleFollowing } = require('../middleware/comment')
+
+const { handleComment, handleFollowing, handlePost } = require('../middleware/comment')
 
 v1Route.param('model', (req, res, next) => {
      const modelName = req.params.model;
@@ -35,6 +35,7 @@ v1Route.get('/getRelation/:collection/:module/:idCollection', middleware.handleG
 // comment
 v1Route.post('/comment', handleComment);
 v1Route.post('/follow', handleFollowing)
+v1Route.post('/post', handlePost)
 
 
 // v1Route.get('/Notification', middleware.handleNotification);
