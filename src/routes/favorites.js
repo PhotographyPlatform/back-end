b8b82f7994414a0b9df5aec05caca7a9533d9497
ehabsalhi <ehab1salhe@gmail.com>
2023-10-15 01:55:43 +0000

    create favorite page dev.2

--- src/routes/favorites.js
+++ src/routes/favorites.js
@@ -9,69 +9,62 @@ favoritesRoute.post("/favorites", isAuth, handleAddToFavorites);
 
 async function handleAddToFavorites(req, res, next) {
     try {
-        let obj = req.body;
-        obj["userid"] = req.users.userId;
-        const postsAlreadyInFavorites = await modules.favorites.findAll();
-
-        const postsIDs = postsAlreadyInFavorites.map(fav => fav.postid);
-
-        let checker = 0;
-
-        postsIDs.map(id => {
-            if (id === obj.postid) checker++;
+      let obj = req.body;
+      obj["userid"] = req.users.userId;
+      const postsAlreadyInFavorites = await modules.favorites.findAll();
+      
+      const favElemnt = postsAlreadyInFavorites.map((fav) => {return {postid:fav.postid, userid:fav.userid}});
+  
+      const exists = favElemnt.some(fav => fav.userid === obj.userid && fav.postid === obj.postid);
+  
+      if (!exists) {
+        const data = await modules.favorites.create(obj);
+        res.status(201).json({
+          message: req.modelName,
+          data,
         });
-
-        if (checker === 0) {
-            const data = await modules.favorites.create(obj);
-            res.status(201).json({
-                message: req.modelName,
-                data
-            });
-        }
-        res.status(400).json({
-            message: 'post already exist'
-        });
+      }
+      res.status(400).json({
+        message: "post already exist",
+      });
     } catch (err) {
-        next(err);
+      next(err);
     }
+  }
 
-}
-
-favoritesRoute.get('/favorites', isAuth ,handleFavrites);
-
+favoritesRoute.get('/favorites', isAuth, handleFavrites);
 async function handleFavrites(req, res, next) {
     try {
-        const userid = req.users.userId;
-
-        const userFavoritesPostsIDs = await modules.favorites.findAll({
-            where: {
-                userid: userid
-            }
+      const userid = req.users.userId;
+      
+      const userFavoritesPostsIDs = await modules.favorites.findAll({
+        where: {
+          userid: userid,
+        },
+      });
+      
+      const favoritesPostIDs = userFavoritesPostsIDs.map((item) => item.postid);
+  
+      const favorites = [];
+  
+      for (const post of favoritesPostIDs) {
+        const posts = await modules.post.findOne({
+          where: {
+            id: post,
+          },
         });
-
-        const favoritesPostIDs = userFavoritesPostsIDs.map(item => item.postid);
-
-        const favorites = [];
-
-        for (const post of favoritesPostIDs) {
-            console.log(post);
-            const posts = await modules.post.findOne({
-                where: {
-                    id: post
-                }
-            });
-
-            favorites.push(posts);
-        }
-
-        res.status(200).json({
-            favorites: favorites
-        });
-
+  
+        favorites.push(posts);
+      }
+      res.status(200).json({
+        favorites: favorites,
+      });
     } catch (err) {
-        next(err)
+      next(err);
     }
-}
+  }
+
+
 
 favoritesRoute.delete('/favorites/:id' ,isAuth, removeItem)
 
@@ -88,3 +81,68 @@ async function removeItem(req, res) {
 }
 
 module.exports = favoritesRoute;
+
+
+// async function handleAddToFavorites(req, res, next) {
+//     try {
+//         let obj = req.body;
+//         obj["userid"] = req.users.userId;
+//         const postsAlreadyInFavorites = await modules.favorites.findAll();
+
+//         const postsIDs = postsAlreadyInFavorites.map(fav => fav.postid);
+
+//         let checker = 0;
+
+//         postsIDs.map(id => {
+//             if (id === obj.postid) checker++;
+//         });
+
+//         if (checker === 0) {
+//             const data = await modules.favorites.create(obj);
+//             res.status(201).json({
+//                 message: req.modelName,
+//                 data
+//             });
+//         }
+//         res.status(400).json({
+//             message: 'post already exist'
+//         });
+//     } catch (err) {
+//         next(err);
+//     }
+
+// }
+
+// async function handleFavrites(req, res, next) {
+//     try {
+//         const userid = req.users.userId;
+
+//         const userFavoritesPostsIDs = await modules.favorites.findAll({
+//             where: {
+//                 userid: userid
+//             }
+//         });
+
+//         const favoritesPostIDs = userFavoritesPostsIDs.map(item => item.postid);
+
+//         const favorites = [];
+
+//         for (const post of favoritesPostIDs) {
+//             console.log(post);
+//             const posts = await modules.post.findOne({
+//                 where: {
+//                     id: post
+//                 }
+//             });
+
+//             favorites.push(posts);
+//         }
+
+//         res.status(200).json({
+//             favorites: favorites
+//         });
+
+//     } catch (err) {
+//         next(err)
+//     }
+// }
